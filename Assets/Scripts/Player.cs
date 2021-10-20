f05ae70c6fbd67ac607676f70448d0ac0b9035e7
12-RachelAng <cherryzx04@gmail.com>
2021-10-20 12:16:51 +0800

    Retry Button

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +20,11 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+    }
 
-      
-
+    //Retry Button
+    public void RetryButton()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
